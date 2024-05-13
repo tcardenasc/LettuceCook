ab9ac6c5ca82f40bd1c00d24799ea6dbce36ec09
Tomás Cárdenas <tomas.cardenas@ug.uchile.cl>
2024-05-13 11:05:08 -0400

    feat(enemySpawner): add a spawn limit

--- EnemySpawner.gd
+++ EnemySpawner.gd
@@ -2,11 +2,18 @@ extends Node
 
 @export var mob_scene: PackedScene
 @export var player: CharacterBody2D
+@export var spawn_limit: int
+var spawned = 0
 
+@onready var timer = $Timer
 
 func _on_timer_timeout():
+	if (spawned == spawn_limit):
+		timer.stop()
+		return
 	var mob = mob_scene.instantiate() as Creature
 	mob.target = player
 	var spawn_location = Vector2(4352, -2664)
 	mob.global_position = spawn_location
 	add_child(mob)
+	spawned += 1
