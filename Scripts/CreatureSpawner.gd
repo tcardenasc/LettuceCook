1fc646914d9edee08ea25cc5801eeeee3ba49813
Tomás Cárdenas <tomas.cardenas@ug.uchile.cl>
2024-05-15 17:33:05 -0400

    fix: make creature_spawner a Node2D - now enemies spawn where the spawner is located in the map - also renamed EnemySpawner node to CreatureSpawner for consistency

--- Scripts/CreatureSpawner.gd
+++ Scripts/CreatureSpawner.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 @export var mob_scene: PackedScene
 @export var player: CharacterBody2D
@@ -13,7 +13,5 @@ func _on_timer_timeout():
 		return
 	var mob = mob_scene.instantiate() as Creature
 	mob.target = player
-	var spawn_location = Vector2(4352, -2664)
-	mob.global_position = spawn_location
 	add_child(mob)
 	spawned += 1
