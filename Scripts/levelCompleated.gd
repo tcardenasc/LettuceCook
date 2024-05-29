4635b02162226bbc4938e42ede3fc386785809ce
Karmagro <carlosmgalvezr@gmail.com>
2024-05-29 18:30:49 -0400

    feat(ui): General layout of the upgrade character done
    The general layout for the elements of the "upgrade character" section of the "levelCompleated" scene is done.

--- Scripts/levelCompleated.gd
+++ Scripts/levelCompleated.gd
@@ -1,6 +1,16 @@
 extends Control
 
 @onready var main = $"."
+@onready var saveManager = $SaveManager
+
+var current_gems = 0
+var all_gems_collected = 0
+var brain_defeated = 0
+var eduardo_defeated = 0
+var max_health = 0
+var damage = 0
+var speed = 0	
+var saved_data
 
 var mainTitle = load("res://Scenes/MainTitle.tscn")
 
@@ -8,6 +18,16 @@ var firstLevel = load("res://Scenes/testScene1.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	saved_data = saveManager.load_data()
+	if not saved_data.is_empty():
+		max_health = saved_data.get("max_health",0)
+		damage = saved_data.get("damage", 0)
+		speed = saved_data.get("speed", 0)
+		current_gems = saved_data.get("current_gems", 0)
+		all_gems_collected = saved_data.get("all_gems_collected", 0)
+		brain_defeated = saved_data.get("brain_defeated", 0)
+		eduardo_defeated = saved_data.get("eduardo_defeated", 0)
+	
 	pass # Replace with function body.
 
 
