30621184a12073fe7f3480e4d0d0e64570189d80
Tomás Cárdenas <tomas.cardenas@ug.uchile.cl>
2024-05-31 13:34:07 -0400

    fix: don't change creature parameters in code, just in editor

--- Scripts/eduardo.gd
+++ Scripts/eduardo.gd
@@ -2,7 +2,7 @@ extends Creature
 class_name Eduardo
 
 func _ready():
-	basicDamage = 20
-	MAX_HEALTH = 10
+	#basicDamage = 20
+	#MAX_HEALTH = 10
 	super._ready()
 	# sobreescribir variables, agregar features
